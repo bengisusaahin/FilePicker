df5cacdd08329d4ecbe0de0a5ccfdffd6a0f51fe
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2025-08-09 23:53:51 +0300

    style: set color, theme, type from ui to presentation folder

---
+++ app/src/main/java/com/bengisusahin/filepicker/presentation/theme/Color.kt
@@ -0,0 +1,31 @@
+package com.bengisusahin.filepicker.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Custom colors for file picker
+val FileUploadBlue = Color(0xFF2196F3)
+val FileUploadGreen = Color(0xFF4CAF50)
+val FileUploadRed = Color(0xFFF44336)
+val FileUploadOrange = Color(0xFFFF9800)
+val FileUploadPurple = Color(0xFF9C27B0)
+
+// Background colors
+val LightGray = Color(0xFFF5F5F5)
+val MediumGray = Color(0xFF9E9E9E)
+val DarkGray = Color(0xFF424242)
+val White = Color(0xFFFFFFFF)
+
+// File type colors
+val ImageFileColor = Color(0xFF4CAF50)
+val VideoFileColor = Color(0xFF2196F3)
+val DocumentFileColor = Color(0xFFFF9800)
+val AudioFileColor = Color(0xFF9C27B0)
+val DefaultFileColor = Color(0xFF757575) 
