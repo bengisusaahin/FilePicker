19aea984a60e9e271a633840b9c12eb16552b9de
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2025-08-09 22:49:07 +0300

    chore: add hilt application

---
+++ app/src/main/java/com/bengisusahin/filepicker/FilePickerApplication.kt
@@ -0,0 +1,7 @@
+package com.bengisusahin.filepicker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FilePickerApplication : Application() 
--- app/src/main/java/com/bengisusahin/filepicker/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/filepicker/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.bengisusahin.filepicker
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bengisusahin.filepicker.presentation.navigation.FilePickerNavigation
+import com.bengisusahin.filepicker.presentation.theme.FilePickerTheme
+import com.bengisusahin.filepicker.utils.NotificationHelper
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        
+        // Initialize notification channels
+        NotificationHelper.createNotificationChannels(this)
+        
+        setContent {
+            FilePickerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    FilePickerNavigation()
+                }
+            }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FilePickerTheme {
+        FilePickerNavigation()
     }
 }
